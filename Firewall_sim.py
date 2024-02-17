b1f9d2b3a4e069f169c9fa1c4a72f191c8fbbfd4
João Marcos N.S <72872911+scriptJohnmns@users.noreply.github.com>
2024-02-17 18:06:42 +0000

    Melhorias

--- Firewall_sim.py
+++ Firewall_sim.py
@@ -1,36 +1,32 @@
 import random
 
-check_icon = chr(10004)
-block_icon = chr(128502)
+CHECK_ICON = chr(10004)
+BLOCK_ICON = chr(128502)
 
 def generate_random_ip():
-    return f"192.168.1.{random.randint(0, 20)}"
+    return f"192.168.1.{random.randint(0, 20)}" #gera o octeto dos hosts que serão bloqueados
 
-def check_firewall_rules(ip, rules):
-    for rule_ip, action in rules.items():
-        if ip == rule_ip:
-            return action
-    return f"allow | {check_icon}"
-
+def check_firewall_rules(ip_adress, firewall_rules):
+    return firewall_rules.get(ip_adress, f"allow | {CHECK_ICON}")
 
 def main():
     
     print("| --------- FIREWALL SIMULATOR --------- |")
 
     firewall_rules = {
-        "192.168.1.1" : f"block | {block_icon}",
-        "192.168.1.5" : f"block | {block_icon}",
-        "192.168.1.9" : f"block | {block_icon}",
-        "192.168.1.13" : f"block | {block_icon}",
-        "192.168.1.18" : f"block | {block_icon}"
+        #lista de ips para bloquear
+        "192.168.1.2" : f"block | {BLOCK_ICON}",
+        "192.168.1.5" : f"block | {BLOCK_ICON}",
+        "192.168.1.9" : f"block | {BLOCK_ICON}",
+        "192.168.1.13" : f"block | {BLOCK_ICON}",
+        "192.168.1.18" : f"block | {BLOCK_ICON}"
     }
-    
-    for _ in range(12):
-        ip_address = generate_random_ip()
-        action = check_firewall_rules(ip_address, firewall_rules)
-        random_number = random.randint(0,9999)
-        print(f"IP: {ip_address} -> Action: {action}")
 
+    for _ in range(15):
+        ip_address = generate_random_ip()    
+        action = check_firewall_rules(ip_address, firewall_rules)              
+        print(f"IP: {ip_address} -> Action: {action}")
+        
 
 if __name__ == "__main__" :
     main()
